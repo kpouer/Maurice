0a6e834133945dbce7a9904b5aaf46202c344e59
Matthieu Casanova <public@kpouer.com>
2025-01-01 23:57:58 +0100

    fix bench

--- benches/bench.rs
+++ benches/bench.rs
@@ -2,9 +2,12 @@ use criterion::{criterion_group, criterion_main, Criterion};
 use maurice::hardware::screen::Screen;
 
 fn criterion_benchmark(c: &mut Criterion) {
-    let mut screen = Screen::default();
+    let mut screen = Screen::new(1);
 
-    c.bench_function("min", |b| b.iter(|| screen.get_pixels()));
+    c.bench_function("get_pixels1", |b| b.iter(|| screen.get_pixels()));
+    let mut screen = Screen::new(3);
+
+    c.bench_function("get_pixels3", |b| b.iter(|| screen.get_pixels()));
 }
 
 criterion_group!(benches, criterion_benchmark);
